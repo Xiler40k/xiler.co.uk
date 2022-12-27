81da6aa67dbb29c824b9fd95616fd80bb957f778
alexander hill <alexanderwfhill@gmail.com>
2022-12-27 17:53:24 +0000

    Temp revert changes to fix website while I slowly add them back in

---
+++ README.md
@@ -0,0 +1,15 @@
+# xiler.me
+Website
+
+Open source code for my website, feel free to look through code and use in own projects if you need.
+
+Will be published soon to the actual domain and will hopefully be updated regularly.
+
+**To do:**
+
+- [x] ~Make it so that a h1 tag isn't required to load css in the startup() function~
+- [ ] Edits pages so they actually have stuff on them
+- [x] ~Spread out buttons on top of screen~
+- [ ] Make download button + preview for projects page
+- [ ] Make links page better than just a clickable youtube logo lmfao
+- [x] ~Make sure Rainbow easter egg doesn't ruin the text color~
---
+++ script.js
@@ -0,0 +1,55 @@
+var numberOfClicks = 0;
+var EventActive = true;
+let mode = "light"
+
+function toggleDarkMode() {
+    if (mode == "light") { mode = "dark" } else { mode = "light" }
+    let items = new Array();
+    // Add names of the items you want to add the darkmode tag to here (e.g. if you wanted to add another one called "tag", then change it to ["body", "button", "ul", ".fa.fa-grip-lines", "h1", "li", "tag"])
+    let names = ["body", "button", "ul", ".fa.fa-grip-lines", "h1", "li"]
+    for(let i = 0; i < names.length; i++) {
+        items = [...items, ...document.querySelectorAll(names[i])]
+    }
+    items.forEach(function(item) {
+        item.classList.toggle("dark-mode");
+    });
+}   
+
+
+function startup() {
+    if (localStorage.getItem("theme") != "light") {
+        toggleDarkMode();
+    }
+}
+
+document.getElementById("dark-mode").addEventListener("click", function() {
+    if (EventActive) {
+        numberOfClicks = numberOfClicks + 1;
+        toggleDarkMode();
+        if (numberOfClicks >= 50) {
+            EventActive = false;
+        }
+
+        if (localStorage.getItem("theme") == "dark") {
+            localStorage.setItem("theme", "light");
+        } else {
+            localStorage.setItem("theme", "dark");
+        }
+
+    } else {
+        if (mode == "dark"){
+            toggleDarkMode()
+        }
+        document.body.classList.toggle("rainbow-mode");
+
+    } 
+});
+
+function changeModeText() {
+    var element = document.getElementById("mode-text");
+    if (element.innerHTML.includes("Dark")) {
+        element.innerHTML = ' Light Mode <i class="fas fa-power-off"></i></button> ';
+        } else {
+        element.innerHTML = ' Dark Mode <i class="fas fa-power-off"></i></button> ';
+        }
+    }
