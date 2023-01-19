cac49cd6505b86e36587782cd7b6788ca5875a47
alexander hill <alexanderwfhill@gmail.com>
2023-01-19 11:57:55 +0000

    Major changes, including new project page and bug fixes regarding javascript

--- script.js
+++ script.js
@@ -7,62 +7,43 @@ function toggleDarkMode() {
     if (mode == "light") { mode = "dark" } else { mode = "light" }
     let items = new Array();
     // Add names of the items you want to add the darkmode tag to here (e.g. if you wanted to add another one called "tag", then change it to ["body", "button", "ul", ".fa.fa-grip-lines", "h1", "li", "tag"])
-    let names = ["body", "button", "ul", ".fa.fa-grip-lines", "h1", "li"]
+    let names = ["body", "button", "ul", ".fa.fa-grip-lines", "h1", "li", "h3", ".download-btn"]
     for(let i = 0; i < names.length; i++) {
         items = [...items, ...document.querySelectorAll(names[i])]
     }
     items.forEach(function(item) {
         item.classList.toggle("dark-mode");
     });
-
-    if (document.title.includes("Project")) {
-        project_btn()
-        console.log("Button change")
-    }
-}   
-
-function project_btn() {
-    var element =document.getElementById("download-btn")
-
-    if (localStorage.getItem("theme") == "light") {
-        element.innerHTML = ' <a href="../photos/babychickens.jpg" download="babychickens.jpg"> <button class="dark-mode"> <i class="fa fa-download"></i> Download </button></a> '
-        console.log("Sucess!")
-    } else {
-        element.innerHTML = ' <a href="../photos/babychickens.jpg" download="babychickens.jpg"> <button class="download"> <i class="fa fa-download"></i> Download </button></a> '
-    }
 }
-
 
 
 function startup() {
-    if (localStorage.getItem("theme") == "dark") {
-        toggleDarkMode();
-    } else if (localStorage.getItem("theme") == null) {
-        toggleDarkMode();
-    }
-
-    localStorage.setItem("chickAggressiveness", 0)
 
     var element = document.getElementById("mode-text");
     if (localStorage.getItem("theme") == "dark") {
+        toggleDarkMode();
         element.innerHTML = ' Light Mode <i class="fas fa-power-off"></i></button> ';
-        } else {
+    } else if (localStorage.getItem("theme") == null) {
+        toggleDarkMode();
+        element.innerHTML = ' Light Mode <i class="fas fa-power-off"></i></button> ';
+    } else {
         element.innerHTML = ' Dark Mode <i class="fas fa-power-off"></i></button> ';
-        }
+    }
+    
+
+    if (document.title.includes("404")) {
+    } else {
+        localStorage.setItem("chickAggressiveness", 0)
     }
 
+    
 
-function chicken_agressiveness() {
-    chickenAgressiveness = chickenAgressiveness + 1
-    localStorage.setItem("chickAggressiveness", chickenAgressiveness )
 }
 
-function goHome() {
-    window.location.replace("./")
-}
 
 function chicken_agressiveness() {
-    chickenAgressiveness = chickenAgressiveness + 1
+    chickenAgressiveness = localStorage.getItem("chickAggressiveness")
+    chickenAgressiveness = parseInt(chickenAgressiveness) + 1
     localStorage.setItem("chickAggressiveness", chickenAgressiveness )
 }
 
